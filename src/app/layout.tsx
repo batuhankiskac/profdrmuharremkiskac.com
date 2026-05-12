5b3a44a2b19c2c96d45b117c60a9884d417c48e8
batuhankiskac <kiskacbatuhan@gmail.com>
2026-05-12 16:11:36 +0300

    Google tag manager code added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,19 @@ export default function RootLayout({
     <html lang="tr">
       <head>
         <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5DGZ7QV3');
+            `,
+          }}
+        />
+        <Script
           strategy="afterInteractive"
           src="https://www.googletagmanager.com/gtag/js?id=AW-10815764688"
         />
@@ -52,6 +65,14 @@ export default function RootLayout({
         />
       </head>
       <body className={`${outfit.variable} ${playfair.variable}`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5DGZ7QV3"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <AuthContextProvider>
           <Header />
           {children}
